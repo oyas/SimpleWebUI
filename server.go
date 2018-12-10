4282ff258a4c250b963fed6344a96d0324395795
oyas <poteto83@gmail.com>
2018-12-11 00:26:23 +0900

    引数処理を更新

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"path"
 	"strings"
-	"flag"
 
 	"./parser"
 	"./websocket_server"
@@ -23,15 +25,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL)
 	base := parser.Read(*temppath, false)
 	contents := parser.Read(*markpath, true)
-	fmt.Println(base)
 	output := strings.Replace(base, ContentsKeyword, contents, 1)
-	fmt.Println(output)
 	fmt.Fprintf(w, output)
 }
 
 func main() {
+	// コマンド実行時の引数をパース
 	flag.Parse()
 
+	// 実行ファイルのパス
+	dir := path.Dir(os.Args[0])
+	if *temppath == "static/index.html" {
+		*temppath = dir + "/" + *temppath
+	}
+
+	// 設定を表示
 	fmt.Printf("Markdown path: %s\n", *markpath)
 	fmt.Printf("Template path: %s\n", *temppath)
 	fmt.Printf("url: http://localhost:%d/\n", *port)
@@ -41,14 +49,17 @@ func main() {
 	go hub.Run()
 	go worker.Run(hub)
 
+	println(dir + "/static")
+
 	// routing
 	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static")))) // アクセスされたURLから /static 部分を取り除いてハンドリングする
+	http.Handle("/static/",
+		http.StripPrefix("/static", http.FileServer(http.Dir(dir+"/static")))) // アクセスされたURLから /static 部分を取り除いてハンドリングする
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		websocket_server.ServeWs(hub, w, r)
 	})
 
-	err := http.ListenAndServe(":" + fmt.Sprint(*port), nil)
+	err := http.ListenAndServe(":"+fmt.Sprint(*port), nil)
 	if err != nil {
 		fmt.Println("ListenAndServe: ", err)
 	}
